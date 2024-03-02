3b341469037b2ec0226ba913788985595c18d919
Facundo Herrera <facundhfed@gmail.com>
2024-03-02 12:05:28 +0100

    Details page box styles fixed

--- src/components/ui/Details/DetailsView.vue
+++ src/components/ui/Details/DetailsView.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex flex-col items-center justify-center">
+    <div class="flex flex-col items-center justify-center ">
 
-        <ul class="flex items-center flex-col justify-center">
-            <li class="mt-2 border-b-4" v-for="doc in documents" :key="doc.id">
-                <div class="flex flex-col text-center justify-center mx-auto">
-                   <p class="text-white">ID: <span class=" text-white font-bold">{{ doc.id }}</span></p>
+        <ul class="flex items-center flex-col justify-center w-[100%] ">
+            <li class="mt-2 border-4 p-2 w-[50%]" v-for="doc in documents" :key="doc.id">
+                <div class="flex flex-col text-center justify-center mx-auto ">
+                   <p class="text-white ">ID: <span class=" text-white font-bold">{{ doc.id }}</span></p>
                     <p class="text-white">Bottles Saved: <span class="font-bold text-white">{{ doc.bottlesSaved }}</span> </p>
                     <p class="text-white">Plastic Saved: <span class="font-bold text-white">{{ doc.plasticsSaved
                     }} KG</span> </p>
