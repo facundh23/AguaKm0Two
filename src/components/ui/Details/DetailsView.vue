383901c213587b5426a31786b91870b550534f2d
Facundo Herrera <facundhfed@gmail.com>
2024-03-02 12:49:36 +0100

    Structure changes

--- src/components/ui/Details/DetailsView.vue
+++ src/components/ui/Details/DetailsView.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col items-center justify-center ">
 
         <ul class="flex items-center flex-col justify-center w-[100%] ">
-            <li class="mt-2 border-4 p-2 w-[50%]" v-for="doc in documents" :key="doc.id">
+            <li class="mt-2 border-4 p-2 w-[70%] md:w-[50%]" v-for="doc in documents" :key="doc.id">
                 <div class="flex flex-col text-center justify-center mx-auto ">
                    <p class="text-white ">ID: <span class=" text-white font-bold">{{ doc.id }}</span></p>
                     <p class="text-white">Bottles Saved: <span class="font-bold text-white">{{ doc.bottlesSaved }}</span> </p>
--- src/components/ui/FormValue/FormValue.vue
+++ src/components/ui/FormValue/FormValue.vue
@@ -1,40 +1,45 @@
 <template>
   <div class="w-[80%] flex items-center justify-center gap-2 py-2 flex-col ">
-    <input class="w-full h-10 rounded-lg md:w-[100%] text-center font-bold" v-model="refills" />
-    <div class="flex items-center justify-around w-full flex-col gap-2 md:flex-row">
-      <button class="bg-blue-600 p-4 rounded-lg font-bold w-full md:w-[30%] hover:bg-blue-700 hover:transition-all hover:duration-700" @click="calculateSavings">Calculate</button>
-      <button class="bg-lime-600 p-4 rounded-lg font-bold w-full md:w-[30%] hover:bg-lime-700 hover:transition-all hover:duration-700" @click="saveResults">Save</button>
+    <input class="w-[80%] p-4  rounded-lg md:w-[80%] text-center font-bold" v-model="refills" placeholder="Number of water refills"/>
+    <div class="flex items-center justify-center w-full  gap-2 md:flex-row">
+      <button class="bg-stone-500 p-2 rounded-lg font-bold w-[80%] md:w-[80%] hover:bg-stone-700 hover:scale-105 hover:transition-all hover:duration-700" @click="calculateSavings">Calculate</button>
     </div>
-    <div class="flex gap-4 flex-col w-[100%]">
-      <div class=" p-4 border-black border-2 bg-green-400 rounded-lg flex justify-around font-bold">
-        <p>Bottles 33ml</p>
-        <p>{{ parseFloat(bottlesSaved) }}</p>
+    <div class="flex gap-4 flex-col w-[80%]">
+      <div class=" p-4   bg-green-300 border-4 border-white rounded-lg flex flex-col justify-center items-center font-bold">
+        <div class="flex items-center w-[100%] justify-center gap-2">
+          <p>Bottles</p>
+          <i class="fa-solid fa-arrow-right"></i>
+          <p>{{ parseFloat(bottlesSaved) }}</p>
+        </div>
+        <div class="flex items-center w-[100%] justify-center gap-2">
+          <p>Kg Plastic</p>
+          <i class="fa-solid fa-arrow-right"></i><p>{{ parseFloat(plasticsSaved) }}</p>
+        </div>
+        <div class="flex items-center w-[100%] justify-center gap-2">
+          <p>Kg Carbon</p>
+          <i class="fa-solid fa-arrow-right"></i><p>{{ parseFloat(carbonSaved) }}</p>
+        </div>
       </div>
-      <div class="p-4 border-black border-2 bg-green-400 rounded-lg flex justify-around font-bold">
-        <p>Kg Plastic</p>
-        <p>{{ parseFloat(plasticsSaved) }}</p>
-      </div>
-      <div class="p-4 border-black border-2 bg-green-400 rounded-lg flex justify-around font-bold">
-        <p>Kg Carbon</p>
-        <p>{{ parseFloat(carbonSaved) }}</p>
-      </div>
+      
     </div>
     <div class="flex items-center justify-center gap-4 flex-col w-full">
       <!-- Botón para generar el enlace -->
-      <button class="bg-lime-600 p-2 rounded-lg font-bold w-full hover:cursor-pointer hover:bg-lime-700 hover:transition-all hover:duration-700" v-if="!linkGenerated" @click="generateLink">
+      <button class="bg-lime-600 p-2 rounded-lg font-bold shadow-black shadow-2xl hover:cursor-pointer w-[79%] hover:bg-lime-700 hover:scale-105 hover:transition-all hover:duration-700 md:w-[80%]" v-if="!linkGenerated" @click="generateLink">
         Generate Link
       </button>
+      
       <!-- Botón para copiar el enlace -->
-      <button class="bg-lime-600 p-2 rounded-lg font-bold w-full" v-else @click="copyLink">
+      <button class="bg-yellow-600 p-2 w-[80%] rounded-lg font-bold" v-else @click="copyLink">
         Copy Link
       </button>
-      <!-- División para mostrar el enlace generado -->
       <div v-if="linkGenerated"
-        class="bg-lime-600 p-2 rounded-lg font-bold w-full flex flex-col items-center justify-around gap-2">
+        class="bg-lime-600 p-2 w-[80%] rounded-lg font-bold flex flex-col items-center justify-around gap-2">
         <label class="text-2xl">Impact link:</label>
-        <input class="p-2 rounded-lg w-[100%] text-black text-center" ref="enlaceInput" type="text" v-model="link"
+        <input class="p-2 rounded-lg w-[79%] text-black text-center" ref="enlaceInput" type="text" v-model="link"
           readonly>
       </div>
+      <button class="bg-lime-600 w-[79%] p-2 rounded-lg font-bold  md:w-[80%] hover:bg-lime-700 hover:scale-105 hover:transition-all hover:duration-700" @click="saveResults">Save</button>
+      <!-- División para mostrar el enlace generado -->
     </div>
 
   </div>
@@ -132,11 +137,16 @@ export default {
           plasticsSaved: this.plasticsSaved,
           carbonSaved: this.carbonSaved
         });
-        this.link = `${baseUrl}?${queryParams.toString()}`;
-        this.linkGenerated = true;
+        let fullLink = `${baseUrl}?${queryParams.toString()}`;
+     if (fullLink.length > 25) {
+      this.link = fullLink.substring(0, 25) + '...';
+      } else {
+      this.link = fullLink;
+      }
+     this.linkGenerated = true;
 
-        // Navegar a la página de detalles
-        this.$router.push({name:SharedLink, query:queryParams})
+      // Navegar a la página de detalles
+      this.$router.push({name: 'SharedLink', query: queryParams})
       } else {
         this.$swal({
           title: 'Error!',
