b85cd2e2c3d4123688a0fc075151dfb47277f16c
Facundo Herrera <facundhfed@gmail.com>
2024-03-02 12:00:40 +0100

    Styles in dashboard changed

--- src/components/Dashboard/DashBoardComponent.vue
+++ src/components/Dashboard/DashBoardComponent.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="container w-full h-screen flex flex-col items-center p-2 mx-auto">
-        <h1 class="font-bold underline text-white mb-3 text-3xl text-center">Refill Impact Dashboard</h1>
-        <InputValue />
+        <h1 class="font-bold text-white mb-3 text-3xl italic text-center">Refill Impact Dashboard</h1>
+        <FormValue />
     </div>
 </template>
 
 
 <script>
-import InputValue from '../ui/InputValue/InputValue.vue';
+import FormValue from '../ui/FormValue/FormValue.vue';
 
 export default {
 
     name: 'DashboarComponent',
-    components: { InputValue },
+    components: { FormValue },
 
 }
 
@@ -23,4 +23,4 @@ export default {
         background-color:#00263A
     }
 
-</style>
+</style>../ui/InputValue/FormValue.vue
