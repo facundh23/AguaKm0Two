4677f679df5e5c00f90383e30d3cb064d4f6da2f
Facundo Herrera <facundhfed@gmail.com>
2024-03-06 21:25:51 +0100

    Routes in progress

--- src/router/routes.js
+++ src/router/routes.js
@@ -17,7 +17,7 @@ const routes = [
   {
     path: '/saved/:id',
     name: 'Links',
-    component: DetailsView,
+    component: SharedLink,
     props: route => ({ // Pasar los parámetros de la URL como props
       bottlesSaved: parseInt(route.query.bottlesSaved),
       plasticsSaved: parseFloat(route.query.plasticsSaved),
