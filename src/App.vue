ea627ea1d9e5f12a68db39d1ee37aa881f95e777
Facundo Herrera <facundhfed@gmail.com>
2024-03-07 07:54:54 +0100

    Project working

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 <template>
         <nav class="navbar text-[#00BC70] text-sm w-full flex justify-around">
             <router-link class="font-bold hover:cursor-pointer"  to="/" active-class="active-link">Dashboard</router-link>
-            <router-link class="font-bold hover:cursor-pointer"  to="/details" active-class="active-link">Details</router-link>
+            <router-link class="font-bold hover:cursor-pointer"  to="/details" active-class="active-link">List Shared Links</router-link>
         </nav>
         <router-view /> 
 </template>
